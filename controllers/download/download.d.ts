06f1fb09ee45c8ac69860de647fc047f20e5ae29
dapize <danielpz@outlook.com>
2022-05-23 10:15:28 -0500

    adapting the download for chimera client

---
+++ controllers/download/download.d.ts
@@ -0,0 +1,4 @@
+export interface IMap {
+  realName: string;
+  chimeraName: string;
+}
--- controllers/download/download.ts
+++ controllers/download/download.ts
@@ -1,12 +1,33 @@
 import { Request, Response } from "express";
 import mapsModel from '@models/maps'
 import { resolve } from "path";
+import { readdir } from "fs/promises";
+import { IMap } from './download.d';
 
-export default function ( request: Request, res: Response ) {
-  let mapName = request.path;
-  const filePath = resolve(__basedir, './public/maps/' + mapName);
+let mapsList:IMap[] = [];
+export default async function ( request: Request, res: Response ) {
+  let mapName = request.path.replace('/', '');
+  const mapsPath = resolve(__basedir, './public/maps')
+  let filePath = mapsPath + '/' +  mapName;
+
+  const getFromChimera = request.headers['user-agent']?.toLowerCase().includes('chimera');
+  if ( getFromChimera ) {
+    if ( !mapsList.length ) {
+      const maps = await readdir(mapsPath);
+      mapsList = maps.map( map => ({
+        realName: map,
+        chimeraName: map.toLowerCase()
+      }))
+    }
+    mapName = decodeURIComponent(mapName) + '.zip';
+    const findMap = mapsList.find( ( map: IMap ) => map.chimeraName === mapName );
+    if ( findMap ) filePath = mapsPath + '/' + decodeURIComponent(findMap.realName.replace('.zip', '.map'));
+  }
+
   res.download(filePath, mapName, async (err) => {
     if ( err ) {
+      console.log(mapName);
+      console.log(err);
       res.status(404);
       return;
     }
